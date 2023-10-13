b69e0df68e8ecf5aa9e72fa99068fdb20e5e2a4f
nikoloztutberidze@outlook.com <nikoloztutberidze@outlook.com>
2023-10-14 01:10:59 +0400

    dependency injection in layers

---
+++ Cinema.Infrastructure/InfrastructureServices.cs
@@ -0,0 +1,19 @@
+﻿using Cinema.Domain.Abstracts.RepositoryAbstracts;
+using Cinema.Domain.Abstracts.UnitOfWorkAbstract;
+using Cinema.Infrastructure.Repositories.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cinema.Infrastructure
+{
+    public static class InfrastructureServices
+    {
+        public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICinemaUnitOfWork, CinemaUnitOfWork.CinemaUnitOfWork>();
+
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+
+            return services;
+        }
+    }
+}
